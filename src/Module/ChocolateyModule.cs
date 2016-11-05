fbf8d3fb37de27e455924c809c1216b379ea339d
Andreas Mosti <andreas.mosti@gmail.com>
2016-11-05 11:49:27 +0100

    added chocoPackages functions

--- src/Module/ChocolateyModule.cs
+++ src/Module/ChocolateyModule.cs
@@ -12,12 +12,18 @@ namespace Stratos.Module
 			StaticConfiguration.DisableErrorTraces = false;
 			m_chocoService = chocoService; 
 
-			Get["/chocolateyVersion"] = parameters =>
+			Get["/chocoVersion"] = parameters =>
 			{
 				var chocoVersion = m_chocoService.ChocoVersion();
 			    return Response.AsJson(chocoVersion != Constants.EmptySemanticVersion ? chocoVersion.ToNormalizedString() : "Can't find Chocolatey Version, is Chocolatey installed or missing from path?");
 			};
-			
+
+			Get["/chocoPackages"] = parameters =>
+			{
+				var chocoPackages = m_chocoService.InstalledPackages();
+				return Response.AsJson(chocoPackages);
+			};
+
 		}
 	}
 }
--- src/Service/ChocolateyService.cs
+++ src/Service/ChocolateyService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NuGet;
 using Stratos.Model;
 
@@ -29,7 +30,28 @@ namespace Stratos.Service
 
 		public IEnumerable<NuGetPackage> InstalledPackages()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				return ParsePackagesOutput(m_command.Execute("choco", "list -lo", true, true));
+			}
+			catch (Exception ex) 
+			{
+				return new List<NuGetPackage> { Constants.EmptyNuGetPackage }; 
+			}
+		}
+
+		private IEnumerable<NuGetPackage> ParsePackagesOutput(string outputString) 
+		{
+			var packages = new List<NuGetPackage>();
+			var rawPackageOutput = outputString.Split('\n');
+			foreach (var textString in rawPackageOutput) 
+			{
+				var packageSplit = textString.Split(null);
+				packages.Add(new NuGetPackage { PackageName = packageSplit[0], Version = SemanticVersion.Parse(rawPackageOutput[1]) });
+			}
+			return packages;
+
+
 		}
 	}
 }
