9838237f50e1081b0b8cdb472df3bdb14cfdb611
andmos <andreas.mosti@gmail.com>
2017-05-05 18:51:02 +0200

    added some more tests for index module

--- src/TestStratos/Module/TestIndexModule.cs
+++ src/TestStratos/Module/TestIndexModule.cs
@@ -25,5 +25,31 @@ namespace TestStratos
 			Assert.Equal(HttpStatusCode.OK, result.StatusCode);
 			Assert.True(result.Body.AsString().Contains("GET"));
 		}
+
+		[Fact]
+		public void indexModule_returnsItsOwnRoute() 
+		{
+			var browser = new Browser(new DefaultNancyBootstrapper(), to => to.Accept("application/json"));
+			var result = browser.Get("/api/", with =>
+			{
+				with.HttpRequest(); 
+			});
+			Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+			Assert.True(result.Body.AsString().Contains(@"/api"));
+		}
+
+		[Fact]
+		public void indexModule_returnsPingRoute()
+		{
+			var browser = new Browser(new DefaultNancyBootstrapper(), to => to.Accept("application/json"));
+			var result = browser.Get("/api/", with =>
+			{
+				with.HttpRequest();
+			});
+			Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+			Assert.True(result.Body.AsString().Contains(@"/ping"));
+		}
+
+
 	}
 }
