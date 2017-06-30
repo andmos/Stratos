adcb27668221706eb70ba0a9e154f240683b8340
andmos <andreas.mosti@gmail.com>
2017-06-30 20:29:23 +0200

    added some theorys

--- src/TestStratos/Helper/TestSemanticVersion.cs
+++ src/TestStratos/Helper/TestSemanticVersion.cs
@@ -6,15 +6,31 @@ namespace TestStratos.Helper
 {
 	public class TestSemanticVersion
 	{
-	    [Fact]
-	    public void SemanticVersion_CheckIfJsonSerializationIsCorrect()
+		[Theory]
+		[InlineData(1, 2, 3, 5)]
+		[InlineData(3, 5, 7, 2)]
+		public void SemanticVersion_CheckIfJsonSerializationIsCorrect(int mayor, int minor, int build, int revision)
 	    {
-		    var semantic = new SemanticVersion(1, 2, 3, 5);
+			var semantic = new SemanticVersion(mayor, minor, build, revision);
 
 		    var semanticJson = JsonConvert.SerializeObject(semantic);
 		    var deserializedSemanticVersion = JsonConvert.DeserializeObject<SemanticVersion>(semanticJson);
 
 		    Assert.Equal(semantic.Version, deserializedSemanticVersion.Version);
 	    }
+
+		[Theory]
+		[InlineData("5.2.3")]
+		public void SemanticVersion_CheckIfJsonSerializationIsCorrect(string version)
+		{
+			var semantic = new SemanticVersion(version);
+
+			var semanticJson = JsonConvert.SerializeObject(semantic);
+			var deserializedSemanticVersion = JsonConvert.DeserializeObject<SemanticVersion>(semanticJson);
+
+			Assert.Equal(semantic.Version, deserializedSemanticVersion.Version);
+		}
+
+
 	}
 }
