10f1b1facd4aff1c10b8f7032736559ad4c77a6e
andmos <andreas.mosti@gmail.com>
2018-03-17 19:02:52 +0100

    xbuild is depricated, changed to msbuild

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nuget restore src/Stratos.sln
-xbuild /property:Configuration=Release src/Stratos.sln 
+msbuild /property:Configuration=Release src/Stratos.sln 
 
 echo "Running tests: "
 mono src/packages/xunit.runner.console.2.2.0/tools/xunit.console.exe src/TestStratos/bin/Release/TestStratos.dll
